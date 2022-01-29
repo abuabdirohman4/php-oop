784654544975b102ed7479bd2d4beb51991a2048
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-29 15:42:50 +0700

    #15 namespace

---
+++ 15_namespace/App/Produk/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Produk;
+
+class User {
+    public function __construct()
+    {
+        echo 'ini adalah class Produk '. __CLASS__;
+    }
+}
---
+++ 15_namespace/App/Service/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+class User {
+    public function __construct()
+    {
+        echo 'ini adalah class Service '. __CLASS__;
+    }
+}
